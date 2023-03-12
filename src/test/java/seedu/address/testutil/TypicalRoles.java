747724a2e2f8d29d72dc10fb916f455c22d33bd4
pzaiming <zaiming.pan@gmail.com>
2023-03-12 17:54:56 +0800

    Update tests for deadline

--- src/test/java/seedu/address/testutil/TypicalRoles.java
+++ src/test/java/seedu/address/testutil/TypicalRoles.java
@@ -1,22 +1,13 @@
 package seedu.address.testutil;
 
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.job.Role;
+
+import static seedu.address.logic.commands.CommandTestUtil.*;
 
 /**
  * A utility class containing a list of {@code Role} objects to be used in tests.
@@ -58,10 +49,10 @@ public class TypicalRoles {
     // Manually added - Role's details found in {@code CommandTestUtil}
     public static final Role AMY = new RoleBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
             .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND)
-            .withSalary("4000").withDeadline("2023-10-20").build();
+            .withSalary(VALID_SALARY_AMY).withDeadline(VALID_DEADLINE_AMY).build();
     public static final Role BOB = new RoleBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
-            .withSalary("4000").withDeadline("2023-10-20").build();
+            .withSalary(VALID_SALARY_BOB).withDeadline(VALID_DEADLINE_BOB).build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
